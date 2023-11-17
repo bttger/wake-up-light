b004c675224e5e83d3b1041eee3e771a1a4cf785
Tom Boettger <t.boettger@live.de>
2023-11-17 20:25:08 +0100

    Implement RTC functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,110 @@
 #include <Arduino.h>
+#include <ThreeWire.h>
+#include <RtcDS1302.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/**
+ * --- Function prototypes ---
+ */
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Function to print the date and time
+void printDateTime(const RtcDateTime &dt);
+
+// Function to set the RTC date and time from Unix epoch time
+void setDateTimeFromEpoch(uint32_t epoch);
+
+// Function to save a specific time (e.g., sunrise time) into RTC memory
+void saveSunriseTime(int day, int hour, int minute);
+
+// Function to retrieve the current day's sunrise time
+RtcDateTime getSunriseTime();
+
+/**
+ * --- Constants ---
+ */
+#define DELAY_TIME 2000
+
+/**
+ * --- Global variables ---
+ */
+ThreeWire myWire(11, 12, 10); // DAT/IO, CLK, RST/CE/CS pin connections
+RtcDS1302<ThreeWire> Rtc(myWire);
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial)
+    delay(DELAY_TIME); // time to get serial running
+  Serial.println("Wake-Up-Light");
+
+  Rtc.Begin();
+
+  // Check if the RTC is write protected, disable write protection if it is
+  if (Rtc.GetIsWriteProtected())
+  {
+    Serial.println("RTC was write protected, enabling writing now");
+    Rtc.SetIsWriteProtected(false);
+  }
+
+  // Check if the RTC is running, set the time if it's not
+  if (!Rtc.GetIsRunning())
+  {
+    Serial.println("RTC is not running; starting and setting the time...");
+    Rtc.SetIsRunning(true);
+    RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+    Rtc.SetDateTime(compiled);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  RtcDateTime now = Rtc.GetDateTime();
+  printDateTime(now);
+  Serial.println();
+  delay(DELAY_TIME);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+/**
+ * --- Function definitions ---
+ */
+
+void printDateTime(const RtcDateTime &dt)
+{
+  char datestring[20];
+
+  snprintf_P(datestring,
+             countof(datestring),
+             PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+             dt.Day(),
+             dt.Month(),
+             dt.Year(),
+             dt.Hour(),
+             dt.Minute(),
+             dt.Second());
+  Serial.print(datestring);
+}
+
+void setDateTimeFromEpoch(uint32_t epoch)
+{
+  RtcDateTime dt = RtcDateTime(epoch);
+  Rtc.SetDateTime(dt);
+}
+
+void saveSunriseTime(int day, int hour, int minute)
+{
+  Rtc.SetMemory((uint8_t)0, (uint8_t)day);
+  Rtc.SetMemory((uint8_t)1, (uint8_t)hour);
+  Rtc.SetMemory((uint8_t)2, (uint8_t)minute);
+}
+
+RtcDateTime getSunriseTime()
+{
+  // Retrieve day, hour, and minute from RTC memory
+  int day = Rtc.GetMemory(0);
+  int hour = Rtc.GetMemory(1);
+  int minute = Rtc.GetMemory(2);
+
+  // Create a temporary RtcDateTime object with retrieved values
+  // Assuming the year, month, and second are not important for this use case
+  RtcDateTime sunriseTime = RtcDateTime(0, 1, day, hour, minute, 0);
+  return sunriseTime;
 }
